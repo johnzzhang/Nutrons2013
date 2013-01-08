be3b017ca19258b2d4fffeb435d224be41a62bd7
NutronsPros <NUTRONS_1@155.33.196.155>
2013-01-07 19:11:01 -0500

    Added basic tank drive.

--- src/edu/wpi/first/wpilibj/ultimateascent/commands/CommandBase.java
+++ src/edu/wpi/first/wpilibj/ultimateascent/commands/CommandBase.java
@@ -3,19 +3,19 @@ package edu.wpi.first.wpilibj.ultimateascent.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.ultimateascent.OI;
-import edu.wpi.first.wpilibj.ultimateascent.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.ultimateascent.subsystems.DriveTrain;
 
 /**
  * The base for all commands. All atomic commands should subclass CommandBase.
  * CommandBase stores creates and stores each control system. To access a
  * subsystem elsewhere in your code in your code use CommandBase.exampleSubsystem
- * @author Author
+ * @author John
  */
 public abstract class CommandBase extends Command {
 
     public static OI oi;
     // Create a single static instance of all of your subsystems
-    public static ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+    public static DriveTrain dt = new DriveTrain();
 
     public static void init() {
         // This MUST be here. If the OI creates Commands (which it very likely
@@ -26,7 +26,7 @@ public abstract class CommandBase extends Command {
         oi = new OI();
 
         // Show what command your subsystem is running on the SmartDashboard
-        SmartDashboard.putData(exampleSubsystem);
+        SmartDashboard.putData(dt);
     }
 
     public CommandBase(String name) {
