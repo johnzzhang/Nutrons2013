ca3a1281163c8464db4bd65961dcf2540af54338
NutronsPros <nutronspros@gmail.com>
2013-01-29 20:46:44 -0500

    Added methods to Elevator subsystems.

--- src/edu/wpi/first/wpilibj/ultimateascent/commands/auto/AutoModeFIRSTry.java
+++ src/edu/wpi/first/wpilibj/ultimateascent/commands/auto/AutoModeFIRSTry.java
@@ -2,7 +2,6 @@ package edu.wpi.first.wpilibj.ultimateascent.commands.auto;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.ultimateascent.commands.drivetrain.DTMoveToFrontOfPyramid;
-import edu.wpi.first.wpilibj.ultimateascent.commands.drivetrain.DTMoveToSetDistance;
 import edu.wpi.first.wpilibj.ultimateascent.commands.loader.LoaderBoltPositionCmd;
 
 /**
@@ -13,7 +12,6 @@ public class AutoModeFIRSTry extends CommandGroup {
     
     public AutoModeFIRSTry() {
         addSequential(new DTMoveToFrontOfPyramid());
-        addSequential(new DTMoveToSetDistance(0));
         addSequential(new LoaderBoltPositionCmd());
     }
     
